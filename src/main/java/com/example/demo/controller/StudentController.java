38f7f333ba9a8c7bcb2eaeed41425a9ee0083ea6
lihu <2840119419@qq.com>
2020-12-03 22:48:00 +0800

    springboot sharding-jdbc 自定义分片策略 分页

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.demo.pojo.Student;
 import com.example.demo.service.StudentService;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -36,6 +40,14 @@ public class StudentController {
         }
 
         return "success";
+    }
+
+    @RequestMapping("/get/{page}/{num}")
+    public List<Student> get(@PathVariable("page") int page, @PathVariable("num") int num){
+        QueryWrapper<Student> wrapper=new QueryWrapper<>();
+        wrapper.orderByAsc("age");
+
+        return studentService.page(new Page<>(page,num),wrapper).getRecords();
     }
 }
 
